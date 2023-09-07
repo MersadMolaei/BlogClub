2691aa8d82d98be6ee350e3afc54a493d71b636b
Mersad <MersadMolaei.dev@gmail.com>
2023-09-07 08:01:43 +0330

    Before: ListView.builder()

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blog_club/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,16 +6,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const defaultFontFamily = 'Avenir';
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final primaryTextColor = Color(0xff0D253C);
+    final secondaryTextColor = Color(0xff2D4379);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Blog Club',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: TextTheme(
+          titleMedium: TextStyle(
+            fontFamily: defaultFontFamily,
+            color: secondaryTextColor,
+            // fontSize: 14,
+          ),
+          titleLarge: TextStyle(
+            fontFamily: defaultFontFamily,
+            fontWeight: FontWeight.bold,
+            color: primaryTextColor,
+          ),
+        ),
       ),
       home: const HomeScreen(),
     );
@@ -26,6 +42,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final themeData = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Hi, Jonathan',
+                        style: themeData.textTheme.titleMedium),
+                    Image.asset(
+                      Assets.img.icons.notification.path,
+                      width: 24,
+                      height: 24,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 0, 24),
+                child: Text("Explore today's",
+                    style: themeData.textTheme.titleLarge),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
