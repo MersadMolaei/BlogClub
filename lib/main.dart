c04eb3f83eb057d52edcd4a1d25153cd64e9019a
Mersad <MersadMolaei.dev@gmail.com>
2023-09-07 03:37:09 +0330

    Starting Point: Place Holder

--- lib/main.dart
+++ lib/main.dart
@@ -16,142 +16,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Mersad'),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Container(
-                  height: 40,
-                  width: 40,
-                  decoration: const BoxDecoration(
-                      color: Colors.deepPurpleAccent,
-                      borderRadius: BorderRadius.all(Radius.circular(13)),
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 7,
-                            color: Colors.grey,
-                            offset: Offset(0, 6))
-                      ]),
-                  child: Center(
-                    child: Text('${_counter + 1}',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall!
-                            .copyWith()
-                            .copyWith(color: Colors.white.withOpacity(0.6))),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: const BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.all(Radius.circular(13)),
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 7,
-                            color: Colors.grey,
-                            offset: Offset(0, 6))
-                      ]),
-                  child: Center(
-                    child: Text(
-                      '$_counter',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineMedium!
-                          .copyWith(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Container(
-                  height: 40,
-                  width: 40,
-                  decoration: const BoxDecoration(
-                      color: Colors.deepPurpleAccent,
-                      borderRadius: BorderRadius.all(Radius.circular(13)),
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 7,
-                            color: Colors.grey,
-                            offset: Offset(0, 6))
-                      ]),
-                  child: Center(
-                    child: Text('${_counter - 1}',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall!
-                            .copyWith()
-                            .copyWith(color: Colors.white.withOpacity(0.6))),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Increment',
-              child: const Icon(Icons.arrow_upward),
-            ),
-            const SizedBox(height: 16),
-            FloatingActionButton(
-              onPressed: _decrementCounter,
-              tooltip: 'Decrement',
-              child: const Icon(Icons.arrow_downward),
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
