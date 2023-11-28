e4d2aae6a7c881488ec1d89b5fcddc646dc7982c
Mersad <MersadMolaei.dev@gmail.com>
2023-11-28 10:16:23 +0330

    Refactor: SplashScreen Images & Icons implementation

--- lib/splash.dart
+++ lib/splash.dart
@@ -20,7 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     // Start the timer when the widget is initialized
     _timer = Timer.periodic(
-      const Duration(milliseconds: 2000), //^ 500
+      const Duration(milliseconds: 1200), //^ 500
       (Timer timer) {
         setState(() {
           // Toggle the value of _showFirstLogo
@@ -39,49 +39,48 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        //* Background image
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.img.background.bg.path), // 1-34
-              fit: BoxFit.cover,
+    return Scaffold(
+      body: Stack(
+        children: [
+          //* Background image
+          Positioned.fill(
+              child: Assets.img.background.bg.image(fit: BoxFit.cover)),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     image: DecorationImage(
+          //       image: AssetImage(Assets.img.background.bg.path), // 1-34
+          //       fit: BoxFit.cover,
+          //     ),
+          //   ),
+          // ),
+          //* Glass Effect
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30), // 30
+            child: Container(
+              color: Colors.grey.withOpacity(0.1),
             ),
           ),
-        ),
-        //* Glass Effect
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30), // 30
-          child: Container(
-            color: Colors.grey.withOpacity(0.1),
+          //* Center LOGO
+          Center(
+            // child: Image.asset(
+            //   Assets.img.icons.usSplash.path,
+            //   width: 230,
+            // ),
+            // child: Image.asset(
+            //   Assets.img.icons.blogSplash.path,
+            //   // width: 230,
+            // ),
+            child: _showFirstLogo
+                ? Assets.img.icons.iran1Splash.image(width: 240)
+                : Assets.img.icons.iranBlackSplash.svg(height: 200),
+
+            // child: SvgPicture.asset(
+            //   Assets.img.icons.iranBlackSplash.path,
+            //   height: 200,
+            // ),
           ),
-        ),
-        //* Center LOGO
-        Center(
-          // child: Image.asset(
-          //   Assets.img.icons.usSplash.path,
-          //   width: 230,
-          // ),
-          // child: Image.asset(
-          //   Assets.img.icons.blogSplash.path,
-          //   // width: 230,
-          // ),
-          child: _showFirstLogo
-              ? Image.asset(
-                  Assets.img.icons.iran1Splash.path,
-                  width: 230,
-                )
-              : SvgPicture.asset(
-                  Assets.img.icons.iranBlackSplash.path,
-                  height: 200,
-                ),
-          // child: SvgPicture.asset(
-          //   Assets.img.icons.iranBlackSplash.path,
-          //   height: 200,
-          // ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
